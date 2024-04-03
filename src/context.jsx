95809f865c8399f7187f6efa111915366056a48e
CR7sewey <miguelmotacarvalho@gmail.com>
2024-04-03 22:49:44 +0100

    Fetching some random data

--- src/context.jsx
+++ src/context.jsx
@@ -1,15 +1,30 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 
 const AppContext = React.createContext();
 
 const AppProvider = ({ children }) => {
-    /**
-     * Cenas da nossa app
-     */
-    const value = "Hello";
+
+    const [receipts, setReceipts] = useState([]);
+    
+    // nao podemos podr async na callback do useEffect!!
+    useEffect(() => {
+        // .then or async/await (fecth api ou axios)
+        const fetchData = async() => {
+            try {
+                const response = await fetch('https://randomuser.me/api/'); // return promise
+                const data = await response.json();
+                console.log(data)
+                return data
+            }
+            catch (error) {
+                console.log(error)
+            }
+        }
+        setReceipts(fetchData());
+    }, []);
 
     return (
-        <AppContext.Provider value={value}>
+        <AppContext.Provider value="" >
             { children }
         </AppContext.Provider>
     )
