3f6e9ec5af49baefb9be40157530dad3613b6ad1
CR7sewey <miguelmotacarvalho@gmail.com>
2024-04-04 21:34:17 +0100

    Meals component - Part 2

--- src/components/Meals.jsx
+++ src/components/Meals.jsx
@@ -1,13 +1,35 @@
 import { useGlobalContext } from "../context";
 
 const Meals = () => {
-    const {meals} = useGlobalContext();
+    const {meals, loading} = useGlobalContext();
     console.log(meals, 'ajajas')
+    if (loading) {
+      return (
+        <section className="section-center">
+          <h4>We are loading the data, please wait a bit...</h4>
+        </section>
+      )
+    }
+    if (meals.length === 0 && loading === false) {
+      return (
+        <section className="section-center">
+          <h4>No data found :(. There are no meals to match your serach. Please try again!</h4>
+        </section>
+      )
+    }
+
     return (
-      <section>
-        {meals.map((obj) => (
-            <h4 key={obj.idMeal}>{obj.idMeal}</h4>
-        ))}
+      <section className="section-center">
+        {meals.map((singleMeal) => {
+            const { idMeal, strMeal: title, strMealThumb: image} = singleMeal;
+            return (<article key={idMeal} className="single-meal" >
+            <img src={image} style={{width: '100%'}} className="img" />
+            <footer>
+              <h5>{title}</h5>
+              <button className="like-btn">Click Me!</button>
+            </footer>
+            </article>)
+        })}
       </section>
     )
 }
--- src/context.jsx
+++ src/context.jsx
@@ -9,24 +9,33 @@ const randomMealsURL = 'https://www.themealdb.com/api/json/v1/1/random.php'
 const AppProvider = ({ children }) => {
 
     const [meals, setMeals] = useState([]);
+    const [loading, setLoading] = useState(true);
     
     // nao podemos podr async na callback do useEffect!!
     useEffect(() => {
         // .then or async/await (fecth api ou axios)
         const fetchMeals = async(url) => {
+            setLoading(true);
             try {
                 const {data}= await axios(url); // return promise
+                if (data.meals ===  null) {
+                    setMeals([]); // no data found !
+                    setLoading(false);
+                    return;
+                }
                 setMeals(data.meals);
+                
             }
             catch (error) {
                 console.log(error.response)
             }
+            setLoading(false);
         }
         fetchMeals(allMealsURL);
     }, []);
 
     return (
-        <AppContext.Provider value={{meals}} >
+        <AppContext.Provider value={{meals, loading}} >
             { children }
         </AppContext.Provider>
     )
